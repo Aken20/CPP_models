ee5801ac2b9d65946f0f58818cb9a9535085a197
aken20 <teentaiten@gmail.com>
2024-07-16 13:49:15 +0400

    -

--- cpp_01/ex03/HumanA.cpp
+++ cpp_01/ex03/HumanA.cpp
@@ -28,8 +28,8 @@ Weapon &HumanA::getWeapon(void)
 };
 void HumanA::attack()
 {
-    if (!this->weapon)
+    if (this->weapon.getType().empty())
         std::cout << this->name << " doesn't have a weapon to attack" << std::endl;
     else
-        std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+        std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
 };
